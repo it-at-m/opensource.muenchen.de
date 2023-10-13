d23e343aeba4b870b7adc39ba4f8c0e29903cdcc
fabian.wilms <fabian.wilms@muenchen.de>
2023-10-13 08:02:42 +0200

    :lipstick: restyling of infobox
    #9

--- .vitepress/theme/Softwarebox.vue
+++ .vitepress/theme/Softwarebox.vue
@@ -1,29 +1,81 @@
 <!--.vitepress/theme/MyLayout.vue-->
 <script setup>
 import DefaultTheme from 'vitepress/theme'
-import { withBase } from 'vitepress'
-const { Layout } = DefaultTheme
+import {useData, withBase} from 'vitepress'
+
+const {Layout} = DefaultTheme
+
+const siteInfo = useData();
+
+const lang = siteInfo.lang.value;
 </script>
 <template>
-  <Layout>
-    <template #aside-top>
-        <div v-if="$frontmatter.licensingmodel" class="infobox">
-            <img v-if="$frontmatter.screenshot"  :src="withBase($frontmatter.screenshot)" />
-            <img v-else-if="$frontmatter.logo"   :src="withBase($frontmatter.logo)" />
-            <dl class="cb">
-                <dt v-if="$frontmatter.code" >Code</dt>
-                <dd v-if="$frontmatter.code" >
-                    <a v-if="$frontmatter.codelinktext" :href="$frontmatter.code">{{ $frontmatter.codelinktext }}</a>
-                    <a v-else :href="$frontmatter.code">{{ $frontmatter.code }}</a>
-                </dd>
-                <dt v-if="$frontmatter.developerlink" >Webseite</dt>
-                <dd v-if="$frontmatter.developerlink" ><a :href="$frontmatter.developerlink">{{ $frontmatter.developerlink }}</a></dd>
-                <dt v-if="$frontmatter.license" >Lizenz</dt>
-                <dd v-if="$frontmatter.license" >{{ $frontmatter.license }}</dd>
-                <dt v-if="$frontmatter.linkapplication" >Anwendung</dt>
-                <dd v-if="$frontmatter.linkapplication" ><a :href="$frontmatter.linkapplication">{{ $frontmatter.linkapplication }}</a></dd>
-            </dl>
-        </div>
-    </template>
-  </Layout>
+    <Layout>
+        <template #aside-top>
+            <div v-if="$frontmatter.licensingmodel" class="infobox">
+                <img v-if="$frontmatter.screenshot" :src="withBase($frontmatter.screenshot)"/>
+                <img v-else-if="$frontmatter.logo" :src="withBase($frontmatter.logo)"/>
+                <div class="pt-4">
+
+                    <v-btn
+                            v-if="$frontmatter.code"
+                            :href="$frontmatter.code"
+                            variant="outlined"
+                            class="mb-2"
+                    >
+                        <v-icon
+                                start
+                                icon="mdi-github"
+                        />
+                        Code
+                    </v-btn>
+
+                    <v-btn
+                            v-if="$frontmatter.developerlink"
+                            :href="$frontmatter.developerlink"
+                            variant="outlined"
+                            class="mb-2"
+                    >
+                        <v-icon
+                                start
+                                icon="mdi-web"
+                        />
+                        Website
+                    </v-btn>
+
+                    <v-btn
+                            v-if="$frontmatter.linkapplication"
+                            :href="$frontmatter.linkapplication"
+                            variant="outlined"
+                            class="mb-2"
+                    >
+                        <v-icon
+                                start
+                                icon="mdi-open-in-app"
+                        />
+
+                        <span v-if="lang === 'en'">Open App</span>
+                        <span v-else>App anzeigen</span>
+                    </v-btn>
+
+                    <div
+                            v-if="$frontmatter.license"
+                    >
+                        <v-tooltip
+                                text="Licence"
+                                location="start"
+                        >
+                            <template v-slot:activator="{ props }">
+                                <div v-bind="props">
+                                    <v-icon icon="mdi-license"/>
+                                    <span>{{ $frontmatter.license }}</span>
+                                </div>
+                            </template>
+                        </v-tooltip>
+                    </div>
+
+                </div>
+            </div>
+        </template>
+    </Layout>
 </template>
