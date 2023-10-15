5e594c3b078a61f4632fae6e511cfb0dda0b14a3
Sebastian <13592751+MrSebastian@users.noreply.github.com>
2023-10-15 17:09:56 +0200

    add i18n to app enhancements

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -5,6 +5,10 @@ import * as components from "vuetify/components"
 import * as directives from "vuetify/directives"
 import "vuetify/styles"
 
+import { createI18n } from "vue-i18n";
+import { messages, DEFAULT_LANG } from "./enhancements/i18n/i18n";
+
+
 import {aliases, mdi} from 'vuetify/iconsets/mdi'
 import '@mdi/font/css/materialdesignicons.css'
 
@@ -23,11 +27,19 @@ const vuetify = createVuetify({
         }
     }
 )
+const i18n = createI18n({
+    legacy: false,
+    fallbackLocale: 'de',
+    messages: messages,
+    locale: DEFAULT_LANG,
+});
 
+console.log('enhace called');
 export default {
     ...DefaultTheme,
     Layout: LhmThemeExtension,
     enhanceApp({app}) {
-        app.use(vuetify)
+        app.use(vuetify);
+        app.use(i18n);
     }
 };
