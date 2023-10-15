9bf33b5f8f119fa13ae4d3cd8f44cdf5e605eb3e
Sebastian <13592751+MrSebastian@users.noreply.github.com>
2023-10-15 17:08:34 +0200

    provide i18n data for tags

---
+++ .vitepress/theme/enhancements/i18n/lang/de.ts
@@ -0,0 +1,24 @@
+import { localizedMessagesType } from "../utility/types";
+
+export const deMessages: localizedMessagesType = {
+    tags: {
+        cicd: "cicd",
+        cms: "cms",
+        client: "client",
+        eigenentwicklung: 'eigententwicklung',
+        foss: "foss",
+        infrastruktur: "infrastruktur",
+        keycloak: "keycloak",
+        kooperation: "kooperation",
+        monitoring: "monitoring",
+        opencore: "opencore",
+        opengovernment: "opengovernment",
+        refarchinfrastruktur: "refarchinfrastruktur",
+        server: "server",
+        schnittstelle: "schnittstelle",
+        sponsor: "sponsor",
+        support: "support",
+        todo: "todo",
+        webanwendung: "webanwendung"
+    }
+};
---
+++ .vitepress/theme/enhancements/i18n/utility/tags.ts
@@ -0,0 +1,22 @@
+export interface Tags {
+    tags: {
+        cicd: string,
+        cms: string,
+        client: string,
+        eigenentwicklung: string,
+        foss: string,
+        infrastruktur: string,
+        keycloak: string,
+        kooperation: string,
+        monitoring: string,
+        opencore: string,
+        opengovernment: string,
+        refarchinfrastruktur: string,
+        server: string,
+        schnittstelle: string,
+        sponsor: string,
+        support: string,
+        todo: string,
+        webanwendung: string,
+    }
+}
