ca4c3ebeebbb7085a6e262db8132e211b17907aa
fabian.wilms <fabian.wilms@muenchen.de>
2023-10-19 09:16:52 +0200

    :lipstick: full lhm logo
    #31

--- .vitepress/theme/LhmThemeExtension.vue
+++ .vitepress/theme/LhmThemeExtension.vue
@@ -2,15 +2,31 @@
 <script setup>
 import DefaultTheme from 'vitepress/theme'
 import SoftwareInfoBox from "./SoftwareInfoBox.vue";
+import {withBase} from "vitepress";
 const {Layout} = DefaultTheme
 </script>
 <template>
     <Layout>
+        <template #nav-bar-content-after>
+            <div class="logo">
+                <a
+                    href="https://www.muenchen.de/"
+                    target="_blank"
+                >
+                    <img
+                        :src="withBase('/public/assets/logo-lhm.png')"
+                        alt="Logo Landeshauptstadt München"
+                    />
+                </a>
+            </div>
+        </template>
+
         <template #aside-top>
             <software-info-box
                 :frontmatter="$frontmatter"
             />
         </template>
+
         <template #doc-before>
             <software-info-box
                 :frontmatter="$frontmatter"
@@ -24,6 +40,26 @@ const {Layout} = DefaultTheme
 <style scoped>
 .mobile {
     display: none;
+}
+
+.logo {
+    display: flex;
+    align-items: center;
+}
+@media (min-width: 768px) {
+    .extra + .logo:before {
+        margin-right: 16px;
+        margin-left: 16px;
+        width: 1px;
+        height: 24px;
+        background-color: var(--vp-c-divider);
+        content: "";
+    }
+}
+
+.logo img {
+    filter: var(--muc-logo-filter);
+    height: 28px;
 }
 
 @media all and (max-width: 1279px) {
