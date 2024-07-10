46e01a43715998a0305de7664411b741a1639368
fabian.wilms <fabian.wilms@muenchen.de>
2024-07-10 10:21:06 +0200

    :lipstick: prettier codeformat

--- .vitepress/theme/enhancements/i18n/lang/en.ts
+++ .vitepress/theme/enhancements/i18n/lang/en.ts
@@ -1,23 +1,23 @@
 import { localizedMessagesType } from "../utility/types";
 
 export const enMessages: localizedMessagesType = {
-    tags: {
-        ai: "AI",
-        application: "Application",
-        development: "Development",
-        eigenentwicklung: "Inhouse",
-        infrastruktur: "Infrastructure",
-        kooperation: "Cooperation",
-        opencore: "Opencore",
-        sponsor: "Sponsor",
-        support: "Support",
-        start2024: "Start 2024",
-        start2023: "Start 2023",
-        start2022: "Start 2022",
-        start2021: "Start 2021",
-        start2020: "Start 2020",
-        k8s: "Kubernetes",
-        saas: "SaaS",
-        devstack: "Development Stack",
-    }
+  tags: {
+    ai: "AI",
+    application: "Application",
+    development: "Development",
+    eigenentwicklung: "Inhouse",
+    infrastruktur: "Infrastructure",
+    kooperation: "Cooperation",
+    opencore: "Opencore",
+    sponsor: "Sponsor",
+    support: "Support",
+    start2024: "Start 2024",
+    start2023: "Start 2023",
+    start2022: "Start 2022",
+    start2021: "Start 2021",
+    start2020: "Start 2020",
+    k8s: "Kubernetes",
+    saas: "SaaS",
+    devstack: "Development Stack",
+  },
 };
